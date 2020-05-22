d1c9630d633accd125ec33d4a367cd448474c058
Randall-Adams <44162048+Randall-Adams@users.noreply.github.com>
2020-05-22 16:26:47 -0400

    Initial Upload
    The original EQOA NPC Maker, which later became known as Kairen

---
+++ EQOA NPC Maker/EQOA NPC Maker/Form1.vb
@@ -0,0 +1,26 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private NPCFile As NPCFile001
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        NPCFile = New NPCFile001
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        CreateFile()
+    End Sub
+
+    Private Sub CreateFile()
+        If TextBox1.Text IsNot "" And _
+            TextBox2.Text IsNot "" Then
+
+            Dim line, file As String
+            Dim sw As New IO.StreamWriter(file)
+            sw.WriteLine(line)
+            sw.Close()
+        End If
+
+    End Sub
+
+End Class
---
+++ EQOA NPC Maker/EQOA NPC Maker/NPCFile001.vb
@@ -0,0 +1,62 @@
+﻿Public Class NPCFile001
+    Private vnameSafe As String
+    Private vnameGame As String
+    Private vX As Integer
+    Private vY As Integer
+    Private vZ As Integer
+    Private vF As Integer
+
+    Public Property nameSafe() As String
+        Get
+            Return vnameSafe
+        End Get
+        Set(value As String)
+            vnameSafe = value
+        End Set
+    End Property
+
+    Public Property nameGame() As String
+        Get
+            Return vnameGame
+        End Get
+        Set(value As String)
+            vnameGame = value
+        End Set
+    End Property
+
+    Public Property X() As String
+        Get
+            Return vX
+        End Get
+        Set(value As String)
+            vX = value
+        End Set
+    End Property
+
+    Public Property Y() As String
+        Get
+            Return vY
+        End Get
+        Set(value As String)
+            vY = value
+        End Set
+    End Property
+
+    Public Property vZ() As String
+        Get
+            Return Z
+        End Get
+        Set(value As String)
+            vZ = value
+        End Set
+    End Property
+
+    Public Property F() As String
+        Get
+            Return vF
+        End Get
+        Set(value As String)
+            vF = value
+        End Set
+    End Property
+End Class
